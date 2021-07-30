2e093a867dc65f1226f0e52eaccd3e6f8e6d149e
Eugen-M <jobmailhtml@gmail.com>
2021-07-30 22:51:42 +0300

    Updated new design for desktop

---
+++ js/popup-inputs.js
@@ -0,0 +1,44 @@
+const FloatLabel = (() => {
+  
+  // add active class and placeholder 
+  const handleFocus = (e) => {
+    const target = e.target;
+    target.parentNode.classList.add('popup-block__row-wrap_active');
+    target.setAttribute('placeholder', target.getAttribute('data-placeholder'));
+  };
+  
+  // remove active class and placeholder
+  const handleBlur = (e) => {
+    const target = e.target;
+    if(!target.value) {
+      target.parentNode.classList.remove('active');
+    }
+    target.removeAttribute('placeholder');    
+  };  
+  
+  // register events
+  const bindEvents = (element) => {
+    const floatField = element.querySelector('input');
+    floatField.addEventListener('focus', handleFocus);
+    floatField.addEventListener('blur', handleBlur);    
+  };
+  
+  // get DOM elements
+  const init = () => {
+    const floatContainers = document.querySelectorAll('.popup-block__row-wrap');
+    
+    floatContainers.forEach((element) => {
+      if (element.querySelector('input').value) {
+          element.classList.add('popup-block__row-wrap_active');
+      }      
+      
+      bindEvents(element);
+    });
+  };
+  
+  return {
+    init: init
+  };
+})();
+
+FloatLabel.init();
